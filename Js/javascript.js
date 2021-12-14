44b48c052d1cc82fdd6759549a9af95b55e80386
Grongos <87789087+CdeCinza@users.noreply.github.com>
2021-12-14 12:03:23 -0300

    Quizz

---
+++ Js/javascript.js
@@ -0,0 +1,39 @@
+var resposta = prompt ("Qual Campeão do lol é um Urso?")
+
+//Verifica se a resposta é diferente (!=) de "VoliBear", se for diferente repete a rotina
+while (resposta != "VoliBear"){
+    //abre a tela interativa para poder colocar a senha novamente, ele está salvando o valor que ele recebe do prompt, na variavel resposta
+resposta =  prompt ("Qual Campeão do lol é um Urso?")
+}
+
+var resposta = prompt ("Qual Campeão do lol é um Samurai?")
+
+//Verifica se a resposta é diferente (!=) de "Yasuo", se for diferente repete a rotina
+while (resposta != "Yasuo"){
+    //abre a tela interativa para poder colocar a senha novamente, ele está salvando o valor que ele recebe do prompt, na variavel resposta
+resposta =  prompt ("Qual Campeão do lol é um Samurai?")
+}
+
+var resposta = prompt ("Qual Campeão do lol é um Gordo alcoólatra?")
+
+//Verifica se a resposta é diferente (!=) de "Gragas", se for diferente repete a rotina
+while (resposta != "Gragas"){
+    //abre a tela interativa para poder colocar a senha novamente, ele está salvando o valor que ele recebe do prompt, na variavel resposta
+resposta =  prompt ("Qual Campeão do lol é um Gordo alcoólatra?")
+}
+
+var resposta = prompt ("Qual Campeão do lol deu Tripple kill em uma série da Netflix?")
+
+//Verifica se a resposta é diferente (!=) de "Jinx", se for diferente repete a rotina
+while (resposta != "Jinx"){
+    //abre a tela interativa para poder colocar a senha novamente, ele está salvando o valor que ele recebe do prompt, na variavel resposta
+resposta =  prompt ("Qual Campeão do lol deu Tripple kill em uma série da Netflix?")
+}
+
+var resposta = prompt ("Qual é o Main Oficial do nosso Professor NiloTk?")
+
+//Verifica se a resposta é diferente (!=) de "132", se for diferente repete a rotina
+while (resposta != "Leona"){
+    //abre a tela interativa para poder colocar a senha novamente, ele está salvando o valor que ele recebe do prompt, na variavel resposta
+resposta =  prompt ("Qual é o Main Oficial do nosso Professor NiloTk")
+}
